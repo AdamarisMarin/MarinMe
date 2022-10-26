fcc7bbfe1eeabcfcb1e4b2031793a671c3ba19e3
Adamaris Marin <adamarismarin92@gmail.com>
2022-10-26 17:06:47 -0500

    commit

--- Parcial2/CargarArchivo/01_CargarArchivo.js
+++ Parcial2/CargarArchivo/01_CargarArchivo.js
@@ -1,22 +1,9 @@
-window.onload=function(){
-    document.getElementById("btnajax").addEventListener("click", pedirHeader);
-    document.getElementById("btnreset").addEventListener("click", iniciarHeader);
+/*$(document).ready( function(){
 
-    function pedirHeader(){
-        var solicitud = new XMLHttpRequest();
-        
-        solicitud.onreadystatechange = function(){
+    $("botonAjax").click(presionBoton);
 
-            if(solicitud.readyState == 4 && solicitud.status == 200){
-                document.getElementById("MyDiv").innerHTML = solicitud.responseText;
-            }   
-        };
+    function presionBoton()
+    var =
 
-        solicitud.open("GET","01_CargarArchivo.txt", true);
-        solicitud.send();
-    }
-
-    function iniciarHeader(){
-        document.getElementById("MyDiv").innerHTML = "<h2>Graffiti</h2> <p>Este es uno de los tipos de dibujo que se hace con pinturas de aerosol y, por lo general, en espacios públicos para que llamen la atención. En estos dibujos se expresan todo tipo de sentimientos, aunque debido a la manera en que están expuestos, suelen reflejar las actitudes de los artistas en la sociedad en la que viven.</p>";
-    }
 }
+)*/
