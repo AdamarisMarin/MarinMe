a4fb6f2182cf39227c882310c5cf61f6f040e82c
Adamaris Marin <adamarismarin92@gmail.com>
2022-10-25 20:19:15 -0500

    commit

---
+++ Parcial2/XMLHTTP/01_CargarArchivo.js
@@ -0,0 +1,22 @@
+window.onload=function(){
+    document.getElementById("btnajax").addEventListener("click", pedirHeader);
+    document.getElementById("btnreset").addEventListener("click", iniciarHeader);
+
+    function pedirHeader(){
+        var solicitud = new XMLHttpRequest();
+        
+        solicitud.onreadystatechange = function(){
+
+            if(solicitud.readyState == 4 && solicitud.status == 200){
+                document.getElementById("MyDiv").innerHTML = solicitud.responseText;
+            }   
+        };
+
+        solicitud.open("GET","01_CargarArchivo.txt", true);
+        solicitud.send();
+    }
+
+    function iniciarHeader(){
+        document.getElementById("MyDiv").innerHTML = "<h2>Este es un header nivel 2</h2>";
+    }
+}
