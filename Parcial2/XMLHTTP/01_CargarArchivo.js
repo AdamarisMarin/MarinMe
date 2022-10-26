0ce7811b195b5acd2c07d3d656f5fe2a757335a2
Adamaris Marin <adamarismarin92@gmail.com>
2022-10-26 12:25:18 -0500

    commit 2

--- Parcial2/XMLHTTP/01_CargarArchivo.js
+++ Parcial2/XMLHTTP/01_CargarArchivo.js
@@ -17,6 +17,6 @@ window.onload=function(){
     }
 
     function iniciarHeader(){
-        document.getElementById("MyDiv").innerHTML = "<h2>Este es un header nivel 2</h2>";
+        document.getElementById("MyDiv").innerHTML = "<h2>Graffiti</h2> <p>Este es uno de los tipos de dibujo que se hace con pinturas de aerosol y, por lo general, en espacios públicos para que llamen la atención. En estos dibujos se expresan todo tipo de sentimientos, aunque debido a la manera en que están expuestos, suelen reflejar las actitudes de los artistas en la sociedad en la que viven.</p>";
     }
 }
