efab4260b026d5aae82222e7f42d3a5ffebd2a48
Adamaris Marin <adamarismarin92@gmail.com>
2022-10-31 15:40:05 -0500

    commit

--- Parcial2/AsyncAwait/AsyncAwait.js
+++ Parcial2/AsyncAwait/AsyncAwait.js
@@ -21,8 +21,7 @@ const cargarPeliculas = async() => {
 		const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=192e0b9821564f26f52949758ea3c473&language=es-MX&page=${pagina}`);
 	
 		console.log(respuesta);
-
-		// Si la respuesta es correcta
+		
 		if(respuesta.status === 200){
 			const datos = await respuesta.json();
 			
