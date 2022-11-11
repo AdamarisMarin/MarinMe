390812139c08de8f8fca7f1fd29505818b3b9b88
Adamaris Marin <adamarismarin92@gmail.com>
2022-11-11 12:54:27 -0600

    commit

--- Parcial3/WP/src/index.js
+++ Parcial3/WP/src/index.js
@@ -1,14 +1,14 @@
 import {cuadrado} from './modulo';
-import Swal from 'sweetalert2';
+import swal from 'sweetalert';
 
 console.log("Hello world webpack");
 
 console.log(cuadrado(2));
 
 document.getElementById("btn").addEventListener(click, ()=> {
-Swal.fire(
-    'Good job!',
-    'You clicked the button!',
-    'success'
-  )
+  swal({
+    title: "Good job!",
+    text: "You clicked the button!",
+    icon: "success",
+  })
 })
