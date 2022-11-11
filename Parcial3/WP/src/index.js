be851a52039b44fda3dcabcc2dfc937bbc9ef981
Adamaris Marin <adamarismarin92@gmail.com>
2022-11-11 13:26:55 -0600

    commit

--- Parcial3/WP/src/index.js
+++ Parcial3/WP/src/index.js
@@ -6,9 +6,5 @@ console.log("Hello world webpack");
 console.log(cuadrado(2));
 
 document.getElementById("btn").addEventListener(click, ()=> {
-  swal({
-    title: "Good job!",
-    text: "You clicked the button!",
-    icon: "success",
-  })
+  swal("Hello world!")
 })
