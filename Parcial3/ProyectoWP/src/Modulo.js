35f061f28c17fdce59e730e865c2fa35805dd4f0
Adamaris Marin <adamarismarin92@gmail.com>
2022-11-11 18:20:03 -0600

    commit

--- Parcial3/ProyectoWP/src/Modulo.js
+++
@@ -1,3 +0,0 @@
-const cuadrado = (valor) => { return valor*valor; }
-
-exports.cuadrado=cuadrado;
--- Parcial3/ProyectoWP/src/index.js
+++
@@ -1,5 +0,0 @@
-import {cuadrado} from './Modulo';
-
-console.log("Hello world webpack");
-
-console.log(cuadrado(2));
