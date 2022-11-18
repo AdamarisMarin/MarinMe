26c7f01d9edce877b34ab57765518d3be9dff526
Adamaris Marin <adamarismarin92@gmail.com>
2022-11-18 07:46:46 -0600

    commit

--- Parcial3/mysqli/mysqli.php
+++ Parcial3/mysqli/mysqli.php
@@ -5,12 +5,14 @@ $usuario = "root";
 $password = "";
 
 $con = mysqli_connect($servidor,$usuario,$password,$basedatos) or die("No se puede conectar a localhost");
-
 $consulta = "select * from cliente"; 
 $registros = mysqli_query($con,$consulta) or die("Problemas en el select");
 
-$result = mysqli_fetch_all($registros);
+while($reg = mysqli_fetch_array($registros, MYSQLI_ASSOC)){
+    printf($reg['id'].'&nbsp&nbsp&nbsp'.$reg['nombre'].' '.$reg['apellidos'].'&nbsp&nbsp&nbsp&nbsp&nbsp'.$reg['direccion'].'&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp'.$reg['telefono'].'&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp'.$reg['correo'].'<br>');
+}
 
-mysqli_close($con);
-echo json_encode($result);
+//$result = mysqli_fetch_all($registros);
+//mysqli_close($con);
+//echo json_encode($result);
 ?>
