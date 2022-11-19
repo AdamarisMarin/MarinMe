1fc871aba59e1dd1840540e17aeefa320c8304bf
Adamaris Marin <adamarismarin92@gmail.com>
2022-11-18 18:04:00 -0600

    commit

---
+++ Parcial3/Webpack/src/index.js
@@ -0,0 +1,15 @@
+import {cuadrado} from './modulo';
+import Swal from 'sweetalert2';
+
+console.log("Hello world webpack");
+
+console.log(cuadrado(2));
+
+document.getElementById("btn").addEventListener("click", ()=> {
+Swal.fire(
+    'Good job!',
+    'You clicked the button!',
+    'success'
+  )
+}
+)
