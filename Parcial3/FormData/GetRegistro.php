d57c10d0170a7216db16df43bace69aa82f28ad0
Adamaris Marin <adamarismarin92@gmail.com>
2022-11-20 15:54:45 -0600

    commit

--- Parcial3/FormData/GetRegistro.php
+++ Parcial3/FormData/GetRegistro.php
@@ -1,13 +1,13 @@
 <?php
-//$id = $_POST['id'];
-$idC = "";
+$id = $_POST=['id'];
+//$idC = "";
 $servidor = "localhost";
 $basedatos = "pedidos";
 $usuario = "root";
 $password = "";
 
 $con = mysqli_connect($servidor,$usuario,$password,$basedatos) or die("no se pudo conectar a local host");
-$consulta = "select * from cliente where id=\"$idC\""; 
+$consulta = "select * from cliente where id=\"$id\""; 
 $registros = mysqli_query($con,$consulta) or die ("problemas en el select");
 
 $result = mysqli_fetch_array($registros,MYSQLI_ASSOC);
