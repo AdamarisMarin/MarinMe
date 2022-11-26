8f5d803b7900668011b24101c9f00cca38f3f58d
Adamaris Marin <adamarismarin92@gmail.com>
2022-11-26 13:26:32 -0600

    commit

---
+++ Parcial3/Grid/Consulta.php
@@ -0,0 +1,15 @@
+<?php
+$servidor = "localhost";
+$basedatos = "pedidos";
+$usuario = "root";
+$password = "";
+
+$con = mysqli_connect($servidor,$usuario,$password,$basedatos) or die("No se puede conectar a localhost");
+$consulta = "select * from cliente"; 
+$registros = mysqli_query($con,$consulta) or die("Problemas en el select");
+
+$result = mysqli_fetch_all($registros,MYSQLI_ASSOC);
+
+mysqli_close($con);
+echo json_encode($result);
+?>
--- Parcial3/Grid/ejemplo.js
+++ Parcial3/Grid/ejemplo.js
@@ -1,38 +1,43 @@
 $(document).ready(function(){
 
-var clients = [
-    { "Name": "Otto Clay", "Age": 25, "Country": 1, "Address": "Ap #897-1459 Quam Avenue", "Married": false },
-    { "Name": "Connor Johnston", "Age": 45, "Country": 2, "Address": "Ap #370-4647 Dis Av.", "Married": true },
-    { "Name": "Lacey Hess", "Age": 29, "Country": 3, "Address": "Ap #365-8835 Integer St.", "Married": false },
-    { "Name": "Timothy Henson", "Age": 56, "Country": 1, "Address": "911-5143 Luctus Ave", "Married": true },
-    { "Name": "Ramona Benton", "Age": 32, "Country": 3, "Address": "Ap #614-689 Vehicula Street", "Married": false }
-];
+    const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "http://localhost/MarinMe/Parcial3/Grid/Consulta.php",
+        "method": "GET",
+        "headers": {
+            "Accept": "*/*"
+        }
+      };
+      
+      $.ajax(settings).done(function (response) {
+        let table = JSON.parse(response);
 
-var countries = [
-    { Name: "", Id: 0 },
-    { Name: "United States", Id: 1 },
-    { Name: "Canada", Id: 2 },
-    { Name: "United Kingdom", Id: 3 }
-];
 
-$("#jsGrid").jsGrid({
-    width: "100%",
-    height: "400px",
-
-    inserting: true,
-    editing: true,
-    sorting: true,
-    paging: true,
-
-    data: clients,
-
-    fields: [
-        { name: "Name", type: "text", width: 150, validate: "required" },
-        { name: "Age", type: "number", width: 50 },
-        { name: "Address", type: "text", width: 200 },
-        { name: "Country", type: "select", items: countries, valueField: "Id", textField: "Name" },
-        { name: "Married", type: "checkbox", title: "Is Married", sorting: false },
-        { type: "control" }
-    ]
-});
-});
+    $("#jsGrid").jsGrid({
+        width: "100%",
+        height: "400px",
+ 
+        inserting: true,
+        editing: true,
+        sorting: true,
+        paging: true,
+        data: table,
+ 
+        fields: [
+            { name: "id", type: "number",validate: "required" },
+            { name: "nombre", type: "text"},
+            { name: "apellidos", type: "text"},
+            { name: "direccion", type: "text"},
+            { name: "telefono", type: "text"},
+            { name: "correo", type: "text"},
+            { name: "cantidad", type: "number"},
+            { name: "tipo", type: "text"},
+            { name: "tamano", type: "text"},
+            { name: "entrega", type: "text"},
+            { name: "pago", type: "text"},
+            { type: "control" }
+        ]
+    });
+    });
+})
